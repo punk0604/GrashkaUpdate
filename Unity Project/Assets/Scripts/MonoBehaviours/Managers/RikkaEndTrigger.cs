b6450a9857dfc65d3d9bb6338560c3c64e2110f4
Ashlyn Horton <punkhorton0604@gmail.com>
2025-02-05 22:39:58 -0500

    level 4 complete

--- Unity Project/Assets/Scripts/MonoBehaviours/Managers/RikkaEndTrigger.cs
+++ Unity Project/Assets/Scripts/MonoBehaviours/Managers/RikkaEndTrigger.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BossEndTrigger : MonoBehaviour
+public class RikkaEndTrigger : MonoBehaviour
 {
     //public static SceneLoader Instance;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Level_4");   
+            SceneManager.LoadScene("Outro");   
         }
     }
 }
+
